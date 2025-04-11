0a30c7a3511dc42f11ec205dc4d50f599fe4347b
mothgharenimish <mothgharenimish0@gmail.com>
2025-04-12 00:51:19 +0530

    Initial Commit

---
+++ ResponsiveView2-SwiftUI/ResponsiveView2_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResponsiveView2_SwiftUIApp.swift
+//  ResponsiveView2-SwiftUI
+//
+//  Created by Nimish Mothghare on 12/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ResponsiveView2_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ResponsiveView2-SwiftUITests/ResponsiveView2_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  ResponsiveView2_SwiftUITests.swift
+//  ResponsiveView2-SwiftUITests
+//
+//  Created by Nimish Mothghare on 12/04/25.
+//
+
+import Testing
+@testable import ResponsiveView2_SwiftUI
+
+struct ResponsiveView2_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
